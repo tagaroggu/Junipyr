d8a6cf5ad42731d4c0348612f82e7d317c1ece0d
tagaroggu <58315587+tagaroggu@users.noreply.github.com>
2019-12-31 21:51:39 -0600

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-var message = 'send 480563492026580992 480563492890476568 eee aaa';
-let server = message.split(' ')[1];
-let channel = message.split(' ')[2];
-let messagecontent = message.slice(+server.length+channel.length+7);
-console.log('Server: ' + server);
-console.log('Channel: ' + channel);
-console.log('Message: ' + messagecontent);
